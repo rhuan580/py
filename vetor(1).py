09951882571bd664a8877fa664cb79218526dbb8
rhuan580 <rhuan580@gmail.com>
2024-09-13 19:56:37 -0300

    Create vetor(1).py

---
+++ vetor(1).py
@@ -0,0 +1,17 @@
+vetor = []
+for i in range(15):
+    numero = int(input("Informe o número: "))
+    vetor.append(numero)
+for elemento in vetor:
+    valor = elemento * 3
+    print(valor)
+
+
+'''
+SEGUNDA POSSIBILIDADE
+    leituras = 1
+    while leituras <= 15:
+        numero = int(input("Informe o número: "))
+        vetor.append(numero)
+        leituras = leituras + 1
+'''
