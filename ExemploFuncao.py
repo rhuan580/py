be6929e91093fc3d8de6c8164bb6052a9a27ac4b
rhuan580 <rhuan580@gmail.com>
2024-09-18 19:47:08 -0300

    Create ExemploFuncao.py

---
+++ ExemploFuncao.py
@@ -0,0 +1,30 @@
+produtos = []
+
+def incluir():
+    print("Acessando a função incluir")
+    while True:
+        nome = input("Nome do Produto: ")
+        preco = float (input("Preço: "))
+        cat = input("Categoria: ")
+        p = {}
+        p[ 'nomeprod' ] = nome
+        p[ 'precoprod' ] = preco
+        p[ 'catprod' ] = cat
+        produtos.append(p)
+
+        r = input("Deseja continuar? ")
+        if r=='n' or r=='N':
+            break
+while True:
+    
+    print("Menu")
+    print("1 - Cadastro")
+    print("2 - Relatório")
+    print("0 - Sair")
+    op = int(input("Informe uma opção: "))
+
+    if op == 0:
+        break
+
+    elif op == 1:
+        incluir()
